04d2e77afd0c82264d1db1b9be3a7ba801f02868
Gitarackur <peterosah5@gmail.com>
2023-06-12 10:36:39 +0100

    restructure page into components

--- components/Jumbotron/index.tsx
+++ components/Jumbotron/index.tsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { ArrowLinks } from '../design-system/CustomLinks/Links';
-import { DefaultBullet } from '../design-system/Bullets';
+import React from "react";
+import { ArrowLinks } from "../design-system/CustomLinks/Links";
+import { DefaultBullet } from "../design-system/Bullets";
+import './jumbotron.css';
 
 const Jumbotron = () => {
   return (
-    <section className="ml-auto max-w-[1271px] flex items-center min-h-[700.05px] bg-[url('/images/jumbotron-img.png')]">
-        <div className="mr-auto md:w-[1037px]">
-          <div>
-            <div className="flex flex-wrap items-end text-white">
-              <h1 className="jumbotron-text">YOUR</h1>
-              <p className="ml-8 jumbotron-content-text">
-                CONTENT
-              </p>
-            </div>
-          </div>
-
-          <div className="mt-28 jumbotron-content-section">
-            <h2 className="text-[#F7F7F7] text-[40px] font-[400] leading-[135%]">
-              <span className="font-['Space_Grotesk'] tracking-[0.05em] break-words">Unleash the power of <br/> content creation on </span>
-              <span className="!font-['Test_Söhne'] font-[600] tracking-[0.12em]">VERLITH.</span>
-            </h2>
-
-            <div className="mt-8 mb-8">
-              <ArrowLinks href={'/'}>
-                Read whitepaper
-              </ArrowLinks>
-            </div>
-
-            <div>
-                <DefaultBullet bg='#5FABC5' boxShadowColor='#5FABC5'/>
-            </div>
+    <section className="ml-auto max-w-[1271px] flex items-center min-h-[700.05px] 
+        bg-[url('/images/jumbotron-img.png')] bg-no-repeat"
+    >
+      <div className="mr-auto md:w-[1037px]">
+        <div>
+          <div className="flex flex-wrap items-end text-white">
+            <h1 className="jumbotron-text">YOUR</h1>
+            <p className="ml-8 jumbotron-content-text">CONTENT</p>
           </div>
         </div>
-        
-    </section>
-  )
-}
 
-export default Jumbotron
+        <div className="mt-28 jumbotron-content-section">
+          <h2 className="text-[#F7F7F7] text-[40px] font-[400] leading-[135%]">
+            <span className="font-['Space_Grotesk'] tracking-[0.05em] break-words">
+              Unleash the power of <br /> content creation on{" "}
+            </span>
+            <span className="!font-['Test_Söhne'] font-[600] tracking-[0.12em]">
+              VERLITH.
+            </span>
+          </h2>
+
+          <div className="mt-8 mb-8">
+            <ArrowLinks href={"/"}>Read whitepaper</ArrowLinks>
+          </div>
+
+          <div>
+            <DefaultBullet bg="#5FABC5" boxShadowColor="#5FABC5" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Jumbotron;
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,39 +1,40 @@
-import Link from 'next/link';
-import React from 'react'
-import PrimaryButton from '../design-system/Button/Button';
+import Link from "next/link";
+import React from "react";
+import PrimaryButton from "../design-system/Button/Button";
 
 const Navbar = () => {
   return (
     <nav className="w-full bg-[#17191E99] text-white min-h-[60px] pt-[19px] pb-[21px]">
-        <div className="md:flex md:items-center md:justify-between md:mx-auto max-w-[1104px]">
-          <div>
-            <h1 className="font-['Test_Söhne'] text-[20px] leading-[100%] tracking-[0.1em] text-[#FFFFFF] font-bold">VERLITH</h1>
+      <div className="md:flex md:items-center md:justify-between md:mx-auto max-w-[1104px]">
+        <div>
+          <h1 className="font-['Test_Söhne'] text-[20px] leading-[100%] tracking-[0.1em] text-[#FFFFFF] font-bold">
+            VERLITH
+          </h1>
+        </div>
+
+        <div className="md:flex md:items-center md:justify-between md:-mx-4 p-4 md:p-0">
+          <div className="md:px-4 py-4 md:py-0">
+            {/* <select>
+                <option>explore</option>
+              </select> */}
+            <Link href={"/"}>explore</Link>
           </div>
 
-          <div className="md:flex md:items-center md:justify-between md:-mx-4">
-            <div className="md:px-4">
-              <select>
-                <option>explore</option>
-              </select>
-            </div>
+          <div className="md:px-4 py-4 md:py-0">
+            <Link href={"/"}>NFT</Link>
+          </div>
 
-            <div className="md:px-4">
-              <Link href={"/"}>NFT</Link>
-            </div>
+          <div className="md:px-4 py-4 md:py-0">
+            <Link href={"/"}>Enterprise</Link>
+          </div>
 
-            <div className="md:px-4">
-              <Link href={"/"}>Enterprise</Link>
-            </div>
-
-            <div className="md:px-4">
-              <PrimaryButton>
-                Connect
-              </PrimaryButton>
-            </div>
+          <div className="md:px-4 py-4 md:py-0">
+            <PrimaryButton>Connect</PrimaryButton>
           </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
