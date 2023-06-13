5001ba0082d5649dfed70632073c968cb7c679cc
Gitarackur <peterosah5@gmail.com>
2023-06-13 01:30:45 +0100

    Fixes to homepage

--- components/Jumbotron/index.tsx
+++ components/Jumbotron/index.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { ArrowLinks } from "../design-system/CustomLinks/Links";
 import { DefaultBullet } from "../design-system/Bullets";
-import './jumbotron.css';
+import "./jumbotron.css";
 
 const Jumbotron = () => {
   return (
-    <section className="ml-auto max-w-[1271px] flex items-center min-h-[700.05px] 
-        bg-[url('/images/jumbotron-img.png')] bg-no-repeat"
+    <section
+      className="ml-auto max-w-[1271px] flex items-center min-h-screen h-full md:min-h-[700.05px] 
+        bg-[url('/images/jumbotron-img.png')] bg-cover md:bg-contain bg-no-repeat"
     >
       <div className="mr-auto md:w-[1037px]">
-        <div>
+        <div className="p-3 md:p-0">
           <div className="flex flex-wrap items-end text-white">
-            <h1 className="jumbotron-text">YOUR</h1>
-            <p className="ml-8 jumbotron-content-text">CONTENT</p>
+            <h1 className="font-['Test_Söhne'] leading-[100%] text-center tracking-[0.05em] text-[#ffffff] 
+            font-[900] text-[40px] md:text-[96px]">
+                YOUR
+            </h1>
+            <p className="ml-8 font-['Test_Söhne'] leading-[100%] tracking-[0.05em] text-[30px] md:text-[60px] font-[600]">
+                CONTENT
+            </p>
           </div>
         </div>
 
-        <div className="mt-28 jumbotron-content-section">
-          <h2 className="text-[#F7F7F7] text-[40px] font-[400] leading-[135%]">
+        <div
+          className="mt-14 md:mt-28 flex flex-col items-start 
+          p-3 md:p-[32px_32px_24px] 
+          gap-[32px] bg-[rgba(20,20,20,0.4)]
+        border-[#0f1011] shadow-[0px_0px_16px_rgba(23,23,23,0.05)] backdrop-blur-[16px] rounded-[8px]"
+        >
+          <h2 className="text-[#F7F7F7] text-[20px] md:text-[40px] font-[400] leading-[135%]">
             <span className="font-['Space_Grotesk'] tracking-[0.05em] break-words">
               Unleash the power of <br /> content creation on{" "}
             </span>
--- components/PlansSection/index.tsx
+++ components/PlansSection/index.tsx
@@ -45,20 +45,20 @@ const PlansSection = () => {
 
   return (
     <section className="">
-      <div className="md:flex md:justify-center md:mx-auto max-w-[1104px]">
+      <div className="md:flex md:justify-center md:mx-auto max-w-[1104px] w-full">
         <div className="md:mb-[138.42px]">
           <div className="md:mb-[107px]">
-            <h1 className="font-['Space_Grotesk'] font-[500] text-[48px] leading-[100%] text-[#FFFFFF] block text-center">
+            <h1 className="py-8 md:py-0 font-['Space_Grotesk'] font-[500] text-[30px] md:text-[48px] leading-[100%] text-[#FFFFFF] block text-center">
               the Future of Data Ownership
             </h1>
           </div>
 
           <div className="md:mt-[107px]">
-            <div className="md:flex md:-mx-[22.75px] -my-[22.75px] md:my-0">
+            <div className="md:flex md:flex-wrap md:-mx-[22.75px] md:-my-[22.75px]">
               {plans &&
                 plans.map((curr: any, idx) => {
                   return (
-                    <div className="md:w-4/12 md:px-[22.75px] py-[22.75px]" key={idx}>
+                    <div className="md:w-6/12 lg:w-4/12 md:px-[22.75px] md:py-[22.75px]" key={idx}>
                       <HomeCard data={curr} />
                     </div>
                   );
